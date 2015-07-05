e2b81103e00db303d54a769c3660e1607c2ec77c
Ben West <bewest@gmail.com>
2015-07-05 01:56:49 -0700

    add missing file

---
+++ public/views/account/sites/index.js
@@ -0,0 +1,40 @@
+
+/* global app:true */
+
+(function() {
+  'use strict';
+
+  app = app || {};
+
+  function cloner (template) {
+    function iter (item) {
+      var clone = template.clone(true);
+      var names = clone.find('.v.name');
+      names.filter(':input').val(item.name);
+      names.not(':input').text(item.name);
+      var base = '//' + item.name + '-login.diabetes.watch';
+      clone.find('IMG.status').attr('src', base + '/api/v1/status.png');
+      clone.find('A.v.view').attr('href', base + '/');
+      return clone;
+    }
+
+    return iter;
+  }
+
+  $(document).ready(function() {
+    // app.mainView = new app.MainView();
+    console.log('sites ready');
+    $.get('/account/sites/list.json', function (body, status, xhr) {
+      console.log('args', arguments);
+      console.log('body', body);
+      console.log('xhr', xhr);
+      var template = cloner($('TBODY.template-site TR').clone(true));
+      var root = $('#site-list');
+      body.map(function (site) {
+        var elem = template(site);
+        root.append(elem);
+
+      });
+    });
+  });
+}());
